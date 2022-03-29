5ffd5f933c14efa7a0c26257554968782c8112c0
jimmyv9 <joaopvapv100@yahoo.com.br>
2022-03-29 12:44:17 -0500

    extracting inputs to train our model

--- src/read_metaQA.py
+++ src/read_metaQA.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import gensim.downloader as api
 from gensim.models import KeyedVectors
@@ -45,17 +46,78 @@ def extract_entities(triples):
         i += 1
     return X
 
+def read_qa(filename, model, entities):
+    """
+    Extracts question answer as question answer pairs, where question is
+    represented as an embedding and answers are represented by their entity
+    index
 
+    Input:
+        filename: path to test_qa text files
+        model: loaded pre-trained model with embeddings
+
+    Output:
+        QA: ([q_emb],x_i), where q_emb is the embedding of words in question i
+        and x_i is the entity index for the respective question
+    """
+
+    with open(filename, 'r') as fin:
+        testing_instances = []
+        for line in fin:
+            question, answer = line.split("\t")
+            all_words = parse_entities(question)
+            # Get question embeddings
+            embeddings = []
+            for word in all_words:
+                embeddings.append(model[word])
+            embeddings = np.array(embeddings)
+            q = np.mean(embeddings, axis=1) # mean pooling
+            # Get answer as mapped entities
+            all_answers = answer.split('|')
+            for ans in all_answers:
+                testing_instances.append((q, entities[ans]))
+    return testing_instances
+
+def parse_entities(sentence):
+    """
+    Tokenizes sentence keeping entities together
+    """
+    tokens = []
+    entity_found = True
+    for word in sentence:
+        if entity_found == True:
+            if word[-1] == ']':
+                entity += word[:-1]
+                tokens.append(entity)
+                entity_found = False
+                entity = ""
+            else:
+                entity += word
+        elif word[0] == '[':
+            entity_found = True
+            entity = word[1:]
+        else:
+            tokens.append(word)
+    return tokens
 
 def main():
-    triples = read_triples("MetaQA/kb.txt")
+    print(len(sys.argv))
+    if len(sys.argv) != 3:
+        print("Execute `python read_metaQA.py path_to_kb path_to_trainQA path_to_pretrained_embeddings`")
+        sys.exit(-1)
+    kb_path = sys.argv[1]
+    train_path = sys.argv[2]
+    emb_path = sys.argv[3]
+
+    triples = read_triples(kb_path)
     X = extract_entities(triples)
-    print(len(X))
     R = {t[1] for t in triples} # extract relations
-    print(len(R))
     print(R)
-    model = api.load("word2vec-google-news-300")
-    model.most_similar("glass")
+    model = api.load(emb_path)
+    training_vals = read_qa(test_path, model, X)
+    with open("./to_train.txt", 'w') as fout:
+        for t in training_vals:
+            fout.write(t)
     return 0
 
 
