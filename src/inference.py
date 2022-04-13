f77c526a608eb20969d32ba55971c7e333f55ea6
Ruosen Li <leeruosen@hotmail.com>
2022-04-13 18:43:09 -0500

    1. delete redundant code

--- src/inference.py
+++ src/inference.py
@@ -5,94 +5,14 @@ import numpy as np
 from tqdm import tqdm
 
 import torch
-import torch.nn
+import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
 from torch.utils.data import DataLoader
 
 from read_metaQA import read_metaqa, read_KB, MetaQADataset
 from model import *
-
-def get_logger(file_name=None):
-    """Set log for the whole file
-    Args:
-        file_name(str): the file we want to record the log
-    """
-    logger = logging.getLogger('train')  # set logger's name
-    logger.setLevel(logging.INFO)  # set logger's level
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-    ch = logging.StreamHandler()  # console handler
-    ch.setLevel(logging.INFO)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-    if file_name is not None and '' != file_name:
-        fh = logging.FileHandler(file_name, mode='w')  # file handler, mode rewrite
-        fh.setLevel(logging.INFO)
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
-
-    return logger
-
-def make_model(config, kb_info):
-    """Build model, initalize optimizer and criterion
-    Args:
-        config(dict): from yaml
-        kb_info: M_subj, M_rel, M_obj matrixes where
-            M_subj(matrix): dim (N_T, N_E) where N_T is the number of
-                             triples in the KB
-            M_rel(matrix): dim (N_T, N_R)
-            M_obj(matrix): dim (N_T, N_E)
-    """
-    if 'kb_multihop' == config['task']:
-        model = RefiedKBQA(config['N_W2V'], config['N_R'], config['n_hop'], kb_info)
-    config['lr'] = float(config['lr'])
-    optimizer = optim.AdamW(model.parameters(), lr=config['lr'])
-    #criterion = nn.CrossEntropyLoss()
-    criterion = WeightedSoftmaxCrossEntropyLoss()
-
-    return model, optimizer, criterion
-
-def collate_fn(batch_data):
-    """Gather a batch of data and convert them to proper format
-    Args:
-        batch_data(list): a list of data with length equal to the batch size
-
-    Return:
-        [xs, qs](Tensor, Tensor): two tensors both with size (batch_size, 300)
-        labels(Tensor): a tensor with size (batch_size,)
-    """
-    # [[[batch_data], total numbers of entities], [], ...] -> [batch_data], [total numbers of entities]
-    batch_data = list(zip(*batch_data))
-    batch_data, total_number = batch_data
-    total_number = total_number[0]
-
-    # [[[x, q], label], [], ...] -> [[x, q], [], ...], [label]
-    batch_data = list(zip(*batch_data))
-    inputs, labels = batch_data
-
-    # [[x, q], [], ...] -> [x], [q]
-    xs, qs = list(zip(*inputs))
-
-    # convert list to tensor
-    xs = torch.LongTensor(xs)
-    xs = F.one_hot(xs, num_classes=total_number).type(torch.FloatTensor)
-    qs = torch.cat(qs, dim=0)
-
-    # convert list of label to uniform distribution on labels
-    label_tensor = torch.zeros_like(xs, dtype=torch.float)
-    for idx, label in enumerate(labels):
-        label = torch.tensor(label)
-        label_tensor[idx,:].index_fill_(0, label, 1 / (len(label)))
-    labels = label_tensor
-
-    # freeze for training
-    xs.requires_grad = False
-    qs.requires_grad = False
-    labels.requires_grad = False
-
-    return [xs, qs], labels
+from main import get_logger, make_model, collate_fn
 
 def run(config):
     """The whole test process
@@ -164,7 +84,6 @@ def run(config):
             y_hat, _ = model(*inputs)
         else:
             raise ValueError
-        #y_hat = torch.argmax(y_hat, dim=1)
         y_sort, y_idx = torch.sort(y_hat, dim=-1, descending=True)
         y_idx = y_idx[:, :5].tolist()
         for y_pred, y_true in zip(y_idx, y):
